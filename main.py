cdcc6ede88e3a0d878568e4ced3034da3e4d9c63
drkostas <georgiou.kostas94@gmail.com>
2022-02-14 16:18:26 -0500

    Added arguments for selecting dataset and network as described in the assignment #4 #5
    See get_args() function or type python main.py --help. The values you can specify for the new arguments (--dataset, --network) are the same as the "type" attribute in the yml file. That way, we can specify many datasets and network structures in the yml and select which one to use by providing the appropriate argument.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 import numpy as np
 from src import Configuration, ColorLogger, Neuron, FullyConnectedLayer, NeuralNetwork
+from typing import *
 
 logger = ColorLogger(logger_name='Main', color='yellow')
 
@@ -24,13 +25,17 @@ def get_args() -> argparse.Namespace:
         'required': True,
         'help': "The configuration yml file"
     }
+    required_args.add_argument('-d', '--dataset', required=True,
+                               help="The datasets to train the network on. "
+                                    "Options (defined in yml): [and, xor, class_example]")
+    required_args.add_argument('-n', '--network', required=True,
+                               help="The network configuration to use. "
+                                    "Options (defined in yml): [single_neuron, 2x1_net]")
     required_args.add_argument('-c', '--config-file', **config_file_params)
     # Optional args
     optional_args = parser.add_argument_group('Optional Arguments')
     optional_args.add_argument('-l', '--log', required=False, default='out.log',
                                help="Name of the output log file")
-    optional_args.add_argument('-d', '--debug', action='store_true',
-                               help='Enables the debug log messages')
     optional_args.add_argument("-h", "--help", action="help", help="Show this help message and exit")
 
     return parser.parse_args()
@@ -40,48 +45,45 @@ def main():
     """This is the main function of main.py
 
     Example:
-        python main.py -c confs/main_conf.yml
+        python main.py --dataset xor --network 2x1_net --config confs/main_conf.yml
     """
 
     # Initializing
     args = get_args()
-    ColorLogger.setup_logger(log_path=args.log, debug=args.debug, clear_log=True)
+    ColorLogger.setup_logger(log_path=args.log, clear_log=True)
     # Load the configuration
     config = Configuration(config_src=args.config_file)
     nn_conf = config.get_config('neural-network')[0]['config']
     datasets = config.get_config('dataset')
 
     # ------- Start of Code ------- #
-    for dataset in datasets:
-        dataset_conf = dataset['config']
-        dataset_type = dataset['type']
-        inputs = np.array(dataset_conf['inputs'])
-        outputs = np.array(dataset_conf['outputs'])
-        num_inputs = inputs.shape[1]
-        netWork = NeuralNetwork(num_layers=nn_conf['num_layers'],
-                                num_neurons=nn_conf['num_neurons'],
-                                activations=nn_conf['activations'],
-                                num_inputs=num_inputs,
-                                loss_function=nn_conf['loss_function'],
-                                learning_rate=nn_conf['learning_rate'])
-        # test train
-        logger.nl()  # New line
-        logger.info(f'Training the network on the {dataset_type} dataset')
-        for epoch in range(nn_conf['epochs']):
-            netWork.train(inputs, outputs)
-            loss = netWork.calculate_loss(inputs, outputs)
-            if epoch % 50 == 0:
-                logger.info(f"Epoch: {epoch} Loss: {loss}")
-        # test predictions
-        logger.info(f'Predictions on the {dataset_type} dataset')
-        for inp, outp in zip(inputs, outputs):
-            logger.info(f"True Output: {outp} Prediction: {netWork.calculate(inp)[0]}", color='cyan')
-
+    dataset = next(filter(lambda x: x['type'] == args.dataset, datasets))
+    dataset_conf = dataset['config']
+    dataset_type = dataset['type']
+    inputs = np.array(dataset_conf['inputs'])
+    outputs = np.array(dataset_conf['outputs'])
+    num_inputs = inputs.shape[1]
+    netWork = NeuralNetwork(num_layers=nn_conf['num_layers'],
+                            num_neurons=nn_conf['num_neurons'],
+                            activations=nn_conf['activations'],
+                            num_inputs=num_inputs,
+                            loss_function=nn_conf['loss_function'],
+                            learning_rate=nn_conf['learning_rate'])
+    # test train
+    logger.nl()  # New line
+    logger.info(f'Training the network on the {dataset_type} dataset')
+    for epoch in range(nn_conf['epochs']):
+        netWork.train(inputs, outputs)
+        loss = netWork.calculate_loss(inputs, outputs)
+        if epoch % 50 == 0:
+            logger.info(f"Epoch: {epoch} Loss: {loss}")
+    # test predictions
+    logger.info(f'Predictions on the {dataset_type} dataset')
+    for inp, outp in zip(inputs, outputs):
+        logger.info(f"True Output: {outp} Prediction: {netWork.calculate(inp)[0]}", color='cyan')
 
     # for i in range(len(inputs)):
     #     print(netWork.calculate(inputs[i]))
-
-
 
 
 if __name__ == '__main__':
